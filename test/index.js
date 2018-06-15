d28bb8d0dd474be6bd76211b9db18c8667b8d457
Dan Grossen <grossen@adobe.com>
2018-06-15 13:56:57 -0600

    Refactor for Bourbon (Mobile) schema (#26)
    * First pass at creating a schema for mobile extension packages json; included an example json to validate against
    
    * Remote schema to be referred inside extension.json
    
    * Second pass; slightly more concise by reusing a schema definition
    
    * Bug fix; use singular names for items; correct definition names
    
    * Updates based on PR feedback
    
    * Allow extension developers to add additional schema to validate rules
    
    * Updated to include definitions inside each scheam since remote refs aren't possible
    
    * MOB-9719 Added repositories definitions
    
    * Refactored schemas to use base schema; added simple test for validation
    
    * modify ids to point to launch.adobe.com for now
    
    * rename files based on PR #26 feedback
    
    * correct mobile extension action example
    
    * change data element type from array of strings to object; add missing condition-matcher definition lost in refactor
    
    * Fix data elements object type
    
    * Rename schema ids
    
    * MOB-9940 Remove EW and SW matchers

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const Ajv = require('ajv');
+
+const baseSchema = require('../schemas/extension-package.json');
+const mobileSchema = require('../schemas/extension-package-mobile.json');
+const webSchema = require('../schemas/extension-package-web.json');
+
+const mobileExample = require('./example-extension-mobile.json');
+const webExample = require('./example-extension-web.json');
+
+const ajv = new Ajv();
+ajv.addSchema(baseSchema)
+
+const mobileValidate = ajv.compile(mobileSchema);
+const webValidate = ajv.compile(webSchema);
+
+const mobileIsValid = mobileValidate(mobileExample);
+const webIsValid = webValidate(webExample);
+
+if (!mobileIsValid && webIsValid) {
+  console.log(mobileValidate.errors);
+} else if (!webIsValid) {
+  console.log(webValidate.errors);
+} else {
+  console.log('VALID!');
+}
