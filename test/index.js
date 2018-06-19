09d181b88091cf0ade57398fed86cfec5d11927c
Dan Grossen <grossen@adobe.com>
2018-06-19 17:39:00 -0600

    Extract mobile extension definitions into a separate file and repo (dms-mobile/bourbon-extension-schema) and combine common schemas for now

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
 const Ajv = require('ajv');
 
-const baseSchema = require('../schemas/extension-package.json');
+// const baseSchema = require('../schemas/extension-package.json');
 const mobileSchema = require('../schemas/extension-package-mobile.json');
 const webSchema = require('../schemas/extension-package-web.json');
 
 const mobileExample = require('./example-extension-mobile.json');
 const webExample = require('./example-extension-web.json');
 
-const ajv = new Ajv();
-ajv.addSchema(baseSchema)
+const ajv = new Ajv({schemaId: 'auto'});
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+// ajv.addSchema(baseSchema)
 
 const mobileValidate = ajv.compile(mobileSchema);
 const webValidate = ajv.compile(webSchema);
